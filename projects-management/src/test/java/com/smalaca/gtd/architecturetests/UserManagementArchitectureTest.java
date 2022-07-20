06742a068ed59c817e5e71c7d37410f16a3c1ece
smalaca <ab13.krakow@gmail.com>
2022-07-20 20:30:14 +0200

    verify if user management context follows layered architecture

--- projects-management/src/test/java/com/smalaca/gtd/architecturetests/UserManagementArchitectureTest.java
+++ projects-management/src/test/java/com/smalaca/gtd/architecturetests/UserManagementArchitectureTest.java
@@ -1,9 +1,10 @@
 package com.smalaca.gtd.architecturetests;
 
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 
-public class UserManagementTest {
+public class UserManagementArchitectureTest {
     private static final String PERSISTENCE = "com.smalaca.gtd.usermanagement.persistence..";
     private static final String JAVA = "java..";
     private static final String SPRING_DATA = "org.springframework.data..";
@@ -23,6 +24,19 @@ public class UserManagementTest {
 
                 .as("Persistence layer should be independent")
                 .because("0004-user-management-layered-architecture.md")
+
+                .check(CodeBase.userManagementClasses());
+    }
+
+    @Test
+    void shouldVerifyDependenciesBetweenPackages() {
+        Architectures.layeredArchitecture()
+                .layer("Persistence").definedBy(PERSISTENCE)
+                .layer("Domain").definedBy("com.smalaca.gtd.usermanagement.domain..")
+                .layer("Controller").definedBy("com.smalaca.gtd.usermanagement.controller.rest..")
+
+                .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Domain")
+                .whereLayer("Domain").mayOnlyBeAccessedByLayers("Controller")
 
                 .check(CodeBase.userManagementClasses());
     }
