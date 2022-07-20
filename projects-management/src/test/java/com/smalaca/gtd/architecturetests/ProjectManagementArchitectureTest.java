95a909d66a0a031bdb0a5caacf9252a5c71566e7
smalaca <ab13.krakow@gmail.com>
2022-07-20 19:50:07 +0200

    introduce tests to verify if domain is independent in project management context

---
+++ projects-management/src/test/java/com/smalaca/gtd/architecturetests/ProjectManagementArchitectureTest.java
@@ -0,0 +1,25 @@
+package com.smalaca.gtd.architecturetests;
+
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+
+public class ProjectManagementArchitectureTest {
+    private static final String DOMAIN = "com.smalaca.gtd.projectmanagement.domain..";
+    private static final String JAVA = "java..";
+    private static final String JPA = "javax.persistence..";
+    private static final String APACHE_COMMONS = "org.apache.commons.lang3..";
+    private static final String FINDBUGS = "edu.umd.cs.findbugs.annotations..";
+
+    @Test
+    void domainShouldBeIndependent() {
+        ArchRuleDefinition.classes().that()
+                .resideInAPackage(DOMAIN)
+
+                .should().onlyDependOnClassesThat()
+                .resideInAnyPackage(
+                        JAVA, JPA, APACHE_COMMONS, FINDBUGS,
+                        DOMAIN)
+
+                .check(CodeBase.projectManagementClasses());
+    }
+}
